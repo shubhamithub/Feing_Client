56d908351c294fb85f927570cfe2f6d0aaf869fd
shubhamithub <shubhampandeyitsoftware2022@gmail.com>
2024-07-25 15:11:55 +0530

    commite second

--- src/main/java/com/Feing_Client/self_study/String_Practice.java
+++ src/main/java/com/Feing_Client/self_study/String_Practice.java
@@ -3,10 +3,10 @@ package com.Feing_Client.self_study;
 public class String_Practice {
 
     public static void main(String[] args) {
-        tring str1 = new String("hello");
+        String str1 = new String("hello");
         String str2 = new String("hello");
         String str3 = "hello";
         String str4 = "hello";
-        Str s5=str3.intern();
+        String s5=str3.intern();
     }
 }
